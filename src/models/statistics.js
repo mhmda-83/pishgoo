ab6783071f8d13481ef4cc909399dd8a0819790e
Mohammad MohammadAlian <mralian.dev@gmail.com>
2021-01-16 09:17:52 +0330

    feat: add createdAt and udpatedAt field to statistics model

--- src/models/statistics.js
+++ src/models/statistics.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-	userId: {
-		type: Number,
-		required: true,
+const schema = new mongoose.Schema(
+	{
+		userId: {
+			type: Number,
+			required: true,
+		},
+		chat: {
+			id: { type: Number, required: true },
+			type: { type: String, required: true },
+			title: String,
+		},
 	},
-	chat: {
-		id: { type: Number, required: true },
-		type: { type: String, required: true },
-		title: String,
-	},
-});
+	{ timestamps: true },
+);
 
 const model = mongoose.model('Statistics', schema, 'statistics');
 
