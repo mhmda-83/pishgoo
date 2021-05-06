63412259aed200922cbb4676dfac31b78fb33b75
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-05-06 14:46:03 +0430

    refactor: extract bot death easter egg handler

--- src/bot.js
+++ src/bot.js
@@ -28,36 +28,6 @@ const createBot = (configs) => {
 
 	bot.use(botHandlers);
 
-	bot.hears(/when am i (going to|gonna) (die|bite the dust)/i, (ctx) => {
-		const probability = randomRangeNumber(1, 6);
-		if (probability === 1) {
-			ctx.reply('soon 🤯', {
-				reply_to_message_id: ctx.message.message_id,
-			});
-		} else if (probability === 2) {
-			ctx.replyWithPhoto('https://ibb.co/sJk4mx6', {
-				reply_to_message_id: ctx.message.message_id,
-			});
-		} else if (probability === 3) {
-			ctx.replyWithPhoto('https://ibb.co/7N85wq7', {
-				reply_to_message_id: ctx.message.message_id,
-				caption: "death is currently having a good time\ndon't ruin it for him",
-			});
-		} else if (probability === 4) {
-			ctx.replyWithPhoto('https://ibb.co/nfY2fRS', {
-				reply_to_message_id: ctx.message.message_id,
-			});
-		} else {
-			ctx.reply(
-				'<pre language="javascript">> Uncaught TypeError: God.getDeathDateOf is not a function</pre>',
-				{
-					reply_to_message_id: ctx.message.message_id,
-					parse_mode: 'HTML',
-				},
-			);
-		}
-	});
-
 	bot.hears(/when does quarantine over/i, (ctx) => {
 		const probability = randomRangeNumber(1, 4);
 		if (probability === 1)
--- src/handlers/bot/index.js
+++ src/handlers/bot/index.js
@@ -1,11 +1,17 @@
 const { Composer } = require('telegraf');
 const { predictHandler } = require('./predictHandler');
 const { startHandler } = require('./startHandler');
+const { deathEasterEggHandler } = require('./deathEasterEggHandler');
 
 const botHandlers = new Composer();
 
 botHandlers.start(startHandler);
 
 botHandlers.command('predict', predictHandler);
+
+botHandlers.hears(
+	/when am i (going to|gonna) (die|bite the dust)/i,
+	deathEasterEggHandler,
+);
 
 module.exports = { botHandlers };
